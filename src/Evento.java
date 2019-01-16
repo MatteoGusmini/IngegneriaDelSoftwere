784eda4ebe3a3d2ac6e1b2d7f78a1fbd0783e09e
MatteoGusmini <45758666+MatteoGusmini@users.noreply.github.com>
2019-01-16 15:17:04 +0100

    f

--- src/Evento.java
+++ src/Evento.java
@@ -97,7 +97,7 @@ public class Evento implements Serializable{
 		
 		// Data odierna per effettuare il confronto
 		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		Date date = new Date("12/05/2020");
+		Date date = new Date();
 		
 		
 		ArrayList<Messaggio> messaggiStato = new ArrayList<>();
@@ -109,7 +109,7 @@ public class Evento implements Serializable{
 					stato= "Conclusa";
 				}
 				else{
-					stato="Fallità";
+					stato="Fallita";
 				}
 			}
 		}
@@ -119,7 +119,7 @@ public class Evento implements Serializable{
 					stato= "Conclusa";
 				}
 				else{
-					stato="Fallità";
+					stato="Fallita";
 				}
 			}
 		}
